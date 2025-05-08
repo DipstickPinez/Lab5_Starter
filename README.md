38b112ce1c4cf592c0315d363e532753dd81077b
DipstickPinez <dipstckpinez@gmail.com>
2025-05-08 05:58:15 -0700

    Created 20 unit tests.

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Lia Chai
 https://dipstickpinez.github.io/Lab5_Starter/expose.html
 
 https://dipstickpinez.github.io/Lab5_Starter/explore.html
+
+
+1) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+- No, I would not use a unit test to test the "message" feature of a messaging application. Messaging applications have a lot of moving parts, and requires the interaction of many different features of a messaging app (checking the user's input, the server both receiving and sending the message, displaying the sent/received message). A Unit Test would not be able to easily test the "message" feature because it is hard to isolate and relies on so many other functions/applications.
+2) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+- Yes, a unit test would be appropriate for testing the "max message length". It is easy to isolate, and can be checked and tested individually without impacting other functions of the messaging application.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,92 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+//Test PhoneNumber
+test('363-223-2157 is a valid Phone Number', () => {
+  expect(isPhoneNumber("363-223-2157")).toBe(true);
+});
+
+test('(111)-111-1111 is a valid Phone Number', () => {
+  expect(isPhoneNumber("(111) 111-1111")).toBe(true);
+});
+
+test('1234567890 is not a valid Phone Number', () => {
+  expect(isPhoneNumber("1234567890")).toBe(true);
+});
+
+test('aaa-aaa-aaaa is a valid Phone Number', () => {
+  expect(isPhoneNumber("aaa-aaa-aaaa")).toBe(true);
+});
+
+
+//Test Emails
+test('dougdoug@dougdoug.com is a valid Email', () => {
+  expect(isEmail("dougdoug@dougdoug.com")).toBe(true);
+});
+
+test('gmail@gmail.edu is a valid Email', () => {
+  expect(isEmail("gmail@gmail.edu")).toBe(true);
+});
+
+test('brianHands@google,com is not a valid Email', () => {
+  expect(isEmail("brianHands@google,com")).toBe(true);
+});
+
+test('bottomText@google.comm is not a valid Email', () => {
+  expect(isEmail("bottomText@google.comm")).toBe(true);
+});
+
+//Test Strong Passwords
+test('password123 is a strong password', () => {
+  expect(isStrongPassword("password123")).toBe(true);
+});
+
+test('whetherwewanted is a strong password', () => {
+  expect(isStrongPassword("whetherwewanted")).toBe(true);
+});
+
+test('123@password is not a strong password', () => {
+  expect(isStrongPassword("123@password")).toBe(true);
+});
+
+test('correcthorsebatterystaple is not a strong password', () => {
+  expect(isStrongPassword("correcthorsebatterystaple")).toBe(true);
+});
+
+
+//Test Dates
+test('5/7/2025 is a date', () => {
+  expect(isDate("5/7/2025")).toBe(true);
+});
+
+test('1/1/1999 is a date', () => {
+  expect(isDate("1/1/1999")).toBe(true);
+});
+
+test('1-1-1999 is not a date', () => {
+  expect(isDate("1-1-1999")).toBe(true);
+});
+
+test('aaaaaaa is not a date', () => {
+  expect(isDate("aaaaaaa")).toBe(true);
+});
+
+//Test HexColors
+
+test('#FFF is a Hex Color', () => {
+  expect(isHexColor("#FFF")).toBe(true);
+});
+
+test('5BCEFA is a Hex Color', () => {
+  expect(isHexColor("5BCEFA")).toBe(true);
+});
+
+test('rgb(1, 206, 250) is not a Hex Color', () => {
+  expect(isHexColor("rgb(1, 206, 250)")).toBe(true);
+});
+
+test('hexColor is not a Hex Color', () => {
+  expect(isHexColor("hexColor")).toBe(true);
+});
+
